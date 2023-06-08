62e52d8017e2b68852b3fb2cf69d2baef659ab84
Kazitanvirazad <tanvir.kaziazad@gmail.com>
2023-06-09 02:23:46 +0530

    Initial Spring-Boot project setup

---
+++ src/main/java/com/featuresaccrossprojects/featuresdata/FeaturesAccrossProjectsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.featuresaccrossprojects.featuresdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeaturesAccrossProjectsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeaturesAccrossProjectsSpringBootApplication.class, args);
+	}
+
+}
